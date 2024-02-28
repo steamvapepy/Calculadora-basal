8a000faf15d3d5f5bf4855a153b91b085e268ada
steamvapepy <160681935+steamvapepy@users.noreply.github.com>
2024-02-27 23:57:21 -0300

    calculadora

---
+++ script.js
@@ -0,0 +1,61 @@
+const button = document.getElementById("calcular");
+const input = document.getElementById("peso");
+const RES = document.getElementById("resultado");
+button.addEventListener("click", calcularDosisDeHidratacion);
+input.addEventListener("click", () => {
+  RES.style.display = "none";
+});
+function calcularDosisDeHidratacion() {
+  const PESO = Number(input.value);
+
+  if (PESO <= 30) {
+    console.log(hollidaySegar(PESO));
+    let resultado = hollidaySegar(PESO);
+    let mm = hollidaySegar(PESO) * 1.5;
+    let mante = hollidaySegar(PESO) / 24;
+    RES.innerHTML = `<p>mantenimiento ${resultado}</p>`;
+    RES.innerHTML += `<p>+medio ${mm}</p>`;
+    RES.innerHTML += `<p> cc/hr ${mante}</p>`;
+    RES.style.display = "block";
+  } else {
+    let resultado = superficieCorporal(PESO);
+    RES.innerHTML = `<p>sc*1500= ${ Math.round(resultado*1500) }</p>`;
+    RES.innerHTML += `<p>sc*2000= ${ Math.round(resultado*2000)}</p>`;
+    RES.style.display = "block";
+  }
+
+}
+
+function hollidaySegar(PESO) {
+  if (PESO <= 10) {
+    return PESO * 100;
+  } else if (PESO <= 20) {
+    return 1000 + (PESO - 10) * 50;
+  } else if (PESO <= 30) {
+    return 1000 + 500 + (PESO - 20) * 20;
+  }
+
+  // return "este es el hs :"+ PESO
+}
+
+function superficieCorporal(PESO) {
+  return (PESO * 4 + 7) / (PESO + 90);
+}
+
+// const ERROR = document.getElementById('error');
+// const FLU = document.getElementById('flu');
+// const MAN = document.getElementById('man');
+
+/* function calculardosisdehidratacion() {
+    let PESO = input.value; */
+
+/*  
+    }
+
+    function hollidaysegar(peso) {
+        return "holliday";
+    }
+
+    function superficieCorporal(peso){
+        return "superficie";
+    }*/
